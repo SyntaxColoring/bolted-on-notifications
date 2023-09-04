c731df0d87ca4ec97d4bad4ab58537f108098015
Max Marrone <max@marrone.nyc>
2023-09-03 22:32:13 -0400

    Update WebSocket models.

--- frontend/src/ws-models.ts
+++ frontend/src/ws-models.ts
@@ -1,14 +1,17 @@
 import { z } from "zod"
 
+
 export const requestSchema = z.object({
     requestID: z.string(),
 })
 export type Request = z.infer<typeof requestSchema>
 
+
 export const responseSchema = z.object({
     requestID: z.string(),
 })
 export type Response = z.infer<typeof responseSchema>
+
 
 export const subscribeRequestSchema = requestSchema.extend({
     messageType: z.literal("subscribeRequest"),
@@ -16,20 +19,20 @@ export const subscribeRequestSchema = requestSchema.extend({
 })
 export type SubscribeRequest = z.infer<typeof subscribeRequestSchema>
 
+
 export const subscribeResponseSchema = responseSchema.extend({
     messageType: z.literal("subscribeResponse"),
     subscriptionID: z.string(),
 })
 export type SubscribeResponse = z.infer<typeof subscribeResponseSchema>
 
+
 export const subscriptionNotificationSchema = z.object({
     messageType: z.literal("subscriptionNotification"),
     subscriptionID: z.string(),
 })
+export type SubscriptionNotification = z.infer<typeof subscriptionNotificationSchema>
 
-export const getAllPostsSchema = z.object({
-    data: z.array(z.object({
-        id: z.string(),
-        title: z.string(),
-    }))
-})
+
+export const anyFromServerSchema = z.union([subscribeResponseSchema, subscriptionNotificationSchema])
+export type AnyFromServer = z.infer<typeof anyFromServerSchema>
