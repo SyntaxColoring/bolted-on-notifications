9c0b37da684dfd25848dfa9c894fcd01caf70839
Max Marrone <max@marrone.nyc>
2023-12-28 04:27:56 -0500

    chore(frontend): Install @radix-ui/themes.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./index-legacy.css";
+import "@radix-ui/themes/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Theme } from "@radix-ui/themes";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
+      <Theme>
+        <App />
+        <ReactQueryDevtools />
+      </Theme>
     </QueryClientProvider>
   </React.StrictMode>,
 );
