881b2578d87fc40b1f2ba604ae735fdd84813931
Max Marrone <max@marrone.nyc>
2024-01-07 20:26:14 -0500

    refactor: Make the backend available on the same port as the frontend.

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,2 @@
-export const SOCKETIO_URL = "ws://localhost:8000";
-export const HTTP_BASE_URL = "http://localhost:8000";
+export const SOCKETIO_URL = "/";
+export const HTTP_BASE_URL = "/api";
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Proxy API requests to the backend dev server.
+    proxy: {
+      "/socket.io": {
+        // TODO: Also proxy http:// requests.
+        target: "ws://localhost:8000",
+        ws: true,
+      },
+      "/api": {
+        target: "http://localhost:8000",
+      },
+    },
+  },
 });
